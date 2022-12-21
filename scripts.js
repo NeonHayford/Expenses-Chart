993050d67b5dc5853852dfb85851014a018a91ec
NeonHayford <hayford.lumorvi@gmail.com>
2022-12-21 11:28:28 +0000

    JS scripts commit

---
+++ scripts.js
@@ -0,0 +1,79 @@
+const data = {
+    labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+    datasets: [{
+      label: 'USD $',
+      fontFamily: 'DM Sans',
+  
+      data: [ 17.45, 34.91, 52.36, 31.07, 23.39, 43.28, 25.48],
+      backgroundColor: [
+        'rgba(236, 117, 93, 1)',
+        'rgba(236, 117, 93, 1)',
+        'rgba(118, 181, 188, 1)',
+        'rgba(236, 117, 93, 1)',
+        'rgba(236, 117, 93, 1)',
+        'rgba(236, 117, 93, 1)',
+        'rgba(236, 117, 93, 1)'
+      ],
+      borderWidth: 0,
+      borderRadius: 5,
+      borderSkipped: false,
+      barThickness: 50.36,
+    }]
+  };
+
+  // config 
+  const config = {
+    type: 'bar',
+    data,
+    options: {
+      
+      scales: {
+        y: {
+          beginAtZero: true,
+          backgroundColor: null,
+
+          ticks: {
+           display: false,
+          
+          },
+           grid: {
+            drawTicks:false,
+            drawOnChartArea:false,
+            borderWidth: 0,
+            drawBorder: false,
+            display: false,
+           }
+        },
+        x: {
+          ticks:{
+              display: true,
+              // display: true,
+          }, 
+
+          grid: {
+            drawTicks:false,
+            drawBorder: false,
+            drawOnChartArea: false,
+            display: false,
+            lineWidth: 5,
+          }
+        }
+      },
+      plugins: {
+        legend: {
+          labels: {
+            font: {
+              fontFamily:"'DM Sans'",
+            },
+            boxWidth: 0
+          }
+        }
+      }
+    }
+  };
+
+  // render init block
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
